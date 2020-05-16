701ea5378701fd30b250a69210b6fd9c6e3ebbef
mcrivaro <marcocrivaro@gmail.com>
2020-05-16 14:32:19 +0200

    add function

--- Chapter 6/reverse_string.py
+++ Chapter 6/reverse_string.py
@@ -13,13 +13,42 @@ def reverse_list(char_list):
 
 
 def reverse_string(string):
-    char_list = split(string)
-    reversed_list = reverse_list(char_list)
-    str_reversed = ''.join(reversed_list)
-    return str_reversed
+    if string and len(string) > 2 and type(string) == str:
+        char_list = split(string)
+        reversed_list = reverse_list(char_list)
+        str_reversed = ''.join(reversed_list)
+        return str_reversed
+    else:
+        return None
+
+
+def reverse_string1(string):
+    if string and len(string) > 2 and type(string) == str:
+        char_list = []
+        for char in string:
+            char_list.append(char)
+        reversed_list = []
+        for i in range(len(char_list)-1, -1, -1):
+            reversed_list.append(char_list[i])
+        str_reversed = ''.join(reversed_list)
+        return str_reversed
+    else:
+        return None
+
+
+def reverse_string2(string):
+    if string and len(string) > 2 and type(string) == str:
+        reversed_list = []
+        for i in range(len(string)-1, -1, -1):
+            reversed_list.append(string[i])
+        str_reversed = ''.join(reversed_list)
+        return str_reversed
+    else:
+        return None
 
 
 if __name__ == "__main__":
-    test_string = "This is a string."
-    str_reversed = reverse_string(test_string)
-    print(str_reversed)
+    print("Please enter a string:")
+    test_string = input()
+    str_reversed2 = reverse_string2(test_string)
+    print(str_reversed2)
