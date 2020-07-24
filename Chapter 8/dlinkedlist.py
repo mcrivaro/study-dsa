a7e02a587dd2b9f65dd51cb0958a50d6310bfce8
Crivaro Marco <marco.crivaro@coop.ch>
2020-07-24 12:42:53 +0200

    add doubly linked list and reverse function

--- Chapter 8/dlinkedlist.py
+++ Chapter 8/dlinkedlist.py
@@ -1,20 +1,22 @@
 class Node():
-    def __init__(self, value, next=None):
+    def __init__(self, value, next=None, prev=None):
         self.value = value
         self.next = next
+        self.prev = prev
 
     def __repr__(self):
         return str(self.__dict__)
 
 
-class LinkedList():
+class DoublyLinkedList():
     def __init__(self, value):
-        self.head = Node(value, next=None)
+        self.head = Node(value, next=None, prev=None)
         self.tail = self.head
         self.length = 1
 
     def append(self, value):
         new_node = Node(value)
+        new_node.prev = self.tail
         self.tail.next = new_node
         self.tail = new_node
         self.length += 1
@@ -22,6 +24,7 @@ class LinkedList():
     def prepend(self, value):
         new_node = Node(value)
         new_node.next = self.head
+        self.head.prev = new_node
         self.head = new_node
         self.length += 1
 
@@ -38,12 +41,15 @@ class LinkedList():
         foregoing_node = self.traverse_to_index(index - 1)
         following_node = foregoing_node.next
         foregoing_node.next = new_node
+        following_node.prev = new_node
         new_node.next = following_node
+        new_node.prev = foregoing_node
         self.length += 1
 
     def remove(self, index):
         if index < 1:
             self.head = self.head.next
+            self.head.prev = None
         elif index > self.length - 1:
             self.tail = self.traverse_to_index(self.length - 2)
             self.tail.next = None
@@ -51,6 +57,7 @@ class LinkedList():
             foregoing_node = self.traverse_to_index(index - 1)
             unwanted_node = foregoing_node.next
             following_node = unwanted_node.next
+            following_node.prev = foregoing_node
             foregoing_node.next = following_node
         self.length -= 1
 
@@ -61,19 +68,15 @@ class LinkedList():
         return current_node
 
     def reverse(self):
-        new_previous_node = self.head
-        current_node = self.head.next
-        new_previous_node.next = None
-        self.tail = new_previous_node
-        while current_node.next:
-            regular_next_node = current_node.next
-            current_node.next = new_previous_node
-            if regular_next_node.next is None:
-                self.head = regular_next_node
-                self.head.next = current_node
-                return
-            new_previous_node = current_node
-            current_node = regular_next_node
+        node = self.head
+        while node:
+            buf = node.prev
+            node.prev = node.next
+            node.next = buf
+            node = node.prev
+        buf = self.head
+        self.head = self.tail
+        self.tail = buf
 
     def print_list(self):
         arr = []
@@ -90,17 +93,12 @@ class LinkedList():
 
 
 if __name__ == "__main__":
-    my_linked_list = LinkedList(25)
+    my_linked_list = DoublyLinkedList(25)
     my_linked_list.append(10)
-    my_linked_list.append(13)
     my_linked_list.prepend(150)
-    my_linked_list.append(17)
-    my_linked_list.prepend(15)
-    my_linked_list.print_list()
     my_linked_list.insert(1, 100)
-    my_linked_list.print_list()
-    my_linked_list.remove(5)
-    my_linked_list.remove(0)
+    my_linked_list.remove(2)
     my_linked_list.print_list()
     my_linked_list.reverse()
     my_linked_list.print_list()
+    print(my_linked_list)
