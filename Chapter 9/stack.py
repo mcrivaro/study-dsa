8640a3dce5cc0983436ea0d2551ff2ca0c45fe7e
Crivaro Marco <marco.crivaro@coop.ch>
2020-07-29 15:02:53 +0200

    add array based stack implementation

--- Chapter 9/stack.py
+++ Chapter 9/stack.py
@@ -38,12 +38,47 @@ class Stack():
         return str(self.__dict__)
 
 
+class ArrayStack():
+    # This stack is based on arrays and not on linked lists
+    def __init__(self):
+        self.top = None
+        self.bottom = None
+        self.data = []
+        self.length = 0
+
+    def push(self, value):
+        self.data.append(value)
+        self.top = self.data[self.length - 1]
+        if self.length == 1:
+            self.bottom = self.data[0]
+        self.length += 1
+
+    def pop(self):
+        if self.length < 1:
+            return None
+        popped_val = self.data.pop(self.length - 1)
+        self.length -= 1
+        if self.length < 1:
+            self.bottom = None
+            self.top = None
+        else:
+            self.top = self.data[self.length - 1]
+        return popped_val
+
+    def peek(self):
+        return self.top
+
+    def __repr__(self):
+        return str(self.__dict__)
+
+
 if __name__ == "__main__":
-    my_stack = Stack()
+    # my_stack = Stack()
+    my_stack = ArrayStack()
     my_stack.push('Amazon.com')
     my_stack.push('Google.com')
     my_stack.push('Youtube.com')
     print(my_stack.pop())
-    print(my_stack.pop())
     print(my_stack.peek())
-    print(my_stack)
+    print(my_stack.pop())
+    print(my_stack.pop())
